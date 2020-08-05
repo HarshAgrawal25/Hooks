6570bed25e21f42f61661c2593670452359f299b
Harsh Agrawal <agrawalharsh199925@gmail.com>
2020-08-05 23:44:03 +0530

    1.3 useState

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import './App.css';
 import ClassCounter from './components/ClassCounter';
 import HookCounter from './components/HookCounter';
 import HookCounterTwo from './HookCounterTwo';
+import HookCounterThree from './components/HookCounterThree';
 
 function App() {
   return (
     <div className="App">
-      <HookCounterTwo />
+      <HookCounterThree />
+      {/* <HookCounterTwo /> */}
       {/* <HookCounter /> */}
           {/* <ClassCounter /> */}
     </div>
